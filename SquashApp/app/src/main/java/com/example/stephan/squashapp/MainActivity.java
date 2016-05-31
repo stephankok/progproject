6bf3504f43d0593895ce0979dfb065448bd7a976
stephankok <stephankok33@gmail.com>
2016-05-31 19:37:42 +0200

    main_screen

--- SquashApp/app/src/main/java/com/example/stephan/squashapp/MainActivity.java
+++ SquashApp/app/src/main/java/com/example/stephan/squashapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.stephan.squashapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        try{
+            switch(item.getItemId()){
+                case R.id.reload:
+                    Toast.makeText(MainActivity.this, "Reload", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.admin:
+                    Toast.makeText(MainActivity.this, "Admin", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.menulogo:
+                    Toast.makeText(MainActivity.this, "Logo", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.settings:
+                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        } catch(Exception e){
+            Log.i("Sleep Recorder", e.toString());
+        }
+        return true;
     }
 }
